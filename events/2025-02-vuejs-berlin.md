ddc21d248cf0adcd77edf6bb058c3d4319e63687
Norman <n@koehr.in>
2025-02-04 17:22:14 +0100

    fix NextEvent and update Feb25

--- events/2025-02-vuejs-berlin.md
+++ events/2025-02-vuejs-berlin.md
@@ -1,5 +1,5 @@
 ---
-title: Vue.js // Berlin
+title: Vue.js // Berlin #96
 date: 2025-02-11T19:00:00+02:00
 slug: 2025-02-11-vuejs-berlin
 hero: https://images.lumacdn.com/cdn-cgi/image/format=auto,quality=81,width=640/event-covers/06/4d793884-19fc-4f33-9ee8-82a1e2626da7
@@ -11,6 +11,24 @@ Let's talk about Vue.js the Vue.js ecosystem!
 ## Presentation 📽️
 
 Slots are open!
+
+<details>
+  <summary>
+    Marco Kaiser a.k.a. KaiserKiwi
+    <span title="Talk">🎙</span>
+    <strong>What is Inertia.js and why I would never start a Vue project without it again.</strong>
+  </summary>
+  <p>Inertia is a new approach to building classic server-driven web apps and it allows you to create fully client-side rendered, single-page apps, without the complexity that comes with modern SPAs.</p>
+</details>
+
+<details>
+  <summary>
+    All of us
+    <span title="Topic">🗫</span>
+    <strong>Lets talk about Vuejs Nation, Vuejs 3.6 and other news!</strong>
+  </summary>
+  <p>Vuejs Nation happened end of January, where many Vuejs VIPs talked about upcoming features. Lets talk about, what we learned!</p>
+</details-->
 
 <!--details>
   <summary>
--- useEvents.ts
+++ useEvents.ts
@@ -8,15 +8,11 @@ function happensBefore(date: Date, eventDate: Date) {
   return t1 < t2
 }
 
-function happensAfter(date: Date, eventDate: Date) {
-  const t1 = eventDate.getTime()
-  const t2 = date.getTime()
-
-  return t1 > t2
-}
-
 export default function useEvents() {
   type Event = typeof events[number]
+  events.sort((a, b) => {
+    return a.frontmatter.date.localeCompare(b.frontmatter.date)
+  })
 
   let nextEvent: Event | undefined = undefined
   const pastEvents: Event[] = []
@@ -26,14 +22,13 @@ export default function useEvents() {
 
   for (let event of events) {
     const eventDate = new Date(event.frontmatter.date)
-    if (happensAfter(today, eventDate)) futureEvents.push(event)
-    else if (happensBefore(today, eventDate)) pastEvents.push(event)
+    if (happensBefore(today, eventDate)) {
+      pastEvents.push(event)
+    } else {
+      if (futureEvents.length === 0) nextEvent = event
+      futureEvents.push(event)
+    }
   }
-
-  pastEvents.sort((a,b) => a.frontmatter.date.localeCompare(b.frontmatter.date))
-  futureEvents.sort((a,b) => a.frontmatter.date.localeCompare(b.frontmatter.date) * -1)
-
-  nextEvent = futureEvents[0]
 
   return { nextEvent, pastEvents, futureEvents }
 }
